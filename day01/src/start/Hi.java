f7b797af3f307cd36e6eae398de98cbae10181bf
rang26 <86291629+rang26@users.noreply.github.com>
2021-08-21 18:21:37 +0900

    Add files via upload

---
+++ day01/src/start/Hi.java
@@ -0,0 +1,13 @@
+package start;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1줄");
+		System.out.println("2줄");
+		System.out.println("3줄");
+		
+	}
+
+}
